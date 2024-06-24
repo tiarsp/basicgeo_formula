1d78f61516f8af76c4358b378f3075bc56b90723
tiarsp <22611135@students.uii.ac.id>
2024-06-24 14:36:17 +0700

    Initial commit for Ipsos for data project

---
+++ R/hello.R
@@ -0,0 +1,92 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+luas_pers = function(sisi1,sisi2)
+{luas_pers = sisi1*sisi2
+print(luas_pers)}
+
+kel_pers = function(sisi)
+{kel_pers = 4*sisi
+print(kel_pers)}
+
+
+luas_pp = function(panjang,lebar)
+{luas_pp = panjang*lebar
+print(luas_pp)}
+
+kel_pp = function(panjang,lebar)
+{kel_pp = 2*(panjang+lebar)
+print(kel_pp)}
+
+
+luas_seg3 = function(alas,tinggi)
+{luas_seg3=(1/2)*alas*tinggi
+print(luas_seg3)}
+
+kel_seg3 = function(alas,sisi1,sisi2)
+{kel_seg3=alas+sisi1+sisi2
+print(kel_seg3)}
+
+
+luas_lingk = function(phi,r)
+{luas_lingk = phi*r^2
+print(luas_lingk)}
+
+kel_lingk = function(phi,r)
+{kel_lingk = 2*phi*r
+print(kel_lingk)}
+
+
+luas_trape = function(sisi1,sisi2,tinggi)
+{luas_trape = ((sisi1+sisi2)/2)*tinggi
+print(luas_trape)}
+
+kel_trape = function(sisi1,sisi2,sisi3,sisi4)
+{kel_trape = sisi1+sisi2+sisi3+sisi4
+print(kel_trape)}
+
+
+luas_belkup = function(d1,d2)
+{luas_belkup = 1/2*d1*d2
+print(luas_belkup)}
+
+kel_belkup = function(sisi1,sisi2,sisi3,sisi4)
+{kel_belkup = sisi1+sisi2+sisi3+sisi4
+print(kel_belkup)}
+
+
+luas_jjgj = function(alas, tinggi)
+{luas_jjgj = alas*tinggi
+print(luas_jjgj)}
+
+kel_jjgj = function(sisi1,sisi2,sisi3,sisi4)
+{kel_jjgj = sisi1+sisi2+sisi3+sisi4
+print(kel_jjgj)}
+
+
+luas_laylay = function(d1,d2)
+{luas_laylay = 1/2*d1*d2
+print(luas_laylay)}
+
+kel_laylay = function(sisi1,sisi2,sisi3,sisi4)
+{kel_laylay = sisi1+sisi2+sisi3+sisi4
+print(kel_laylay)}
+kel_laylay(4,7)
+
+
